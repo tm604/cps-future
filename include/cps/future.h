d36d03a2a29eded9452d15b392e9af657fa34504
Tom Molesworth <tom@perlsite.co.uk>
2015-05-31 10:07:28 +0100

    Minor doc updates

--- include/cps/future.h
+++ include/cps/future.h
@@ -11,13 +11,15 @@
 /**
  * \page futures
  *
- * A future represents the base implementation. It provides chaining and sequencing methods,
+ * A base_future represents the base implementation. It provides chaining and sequencing methods,
  * and is able to retain state (pending, failed, cancelled, complete) but does not store
- * values.
+ * values. cps::future is provided as an alias.
  *
  * A leaf_future is a template class which extends future to include the concept of a value.
+ * It introduces get(), and (optionally) provides the result to ->on_done, ->then callbacks.
  *
- * A sequence_future extends future to provide a container for zero or more futures.
+ * A sequence_future is what you get back from ->then or ->else. It retains the inner future.
+ * The actual type of the inner future isn't known until the callback runs.
  */
 
 #include <cps/base_future.h>
