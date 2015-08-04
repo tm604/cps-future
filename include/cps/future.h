afe306cc0cc044d7c7e79cb2a440a1e09f4f8188
Tom Molesworth <tom@perlsite.co.uk>
2015-08-04 18:38:46 +0100

    is_string improvements

--- include/cps/future.h
+++ include/cps/future.h
@@ -33,22 +33,37 @@ template<typename U> typename function_traits<U>::arg_type* arg_type_for(U);
 
 /**
  * A template that indicates true for a string, false for any other type.
+ *
  * The following types are considered to be strings:
+ *
  * * std::string
+ * * std::string &
  * * const std::string
+ * * const std::string &
+ * * const std::string const &
  * * char *
  * * const char *
+ * * char
+ *
+ * That last one might not be entirely obvious or ideal, but for our purposes
+ * (string / exception differentiation) it's acceptable.
  */
 template<typename U>
 class is_string : public std::integral_constant<
 	bool,
 	std::is_same<
-		typename std::remove_cv<U>::type,
+		typename std::remove_cv<
+			typename std::remove_reference<
+				typename std::remove_cv<U>::type
+			>::type
+		>::type,
 		std::string
 	>::value
 	||
 	std::is_same<
-		typename std::remove_cv<typename std::remove_pointer<U>::type>::type,
+		typename std::remove_cv<
+			typename std::remove_pointer<U>::type
+		>::type,
 		char
 	>::value
 > { };
