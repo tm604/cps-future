d87eade5c96827af1fe7ab4812230e64cf64c249
Tom Molesworth <tom@perlsite.co.uk>
2015-09-06 19:12:25 +0100

    Rename ok() macro to CHECK()
    so much UPPER_CASE in the tests anyway the original reason for sticking
    with TAP-style directives no longer applies so much.

--- tests/future.cpp
+++ tests/future.cpp
@@ -6,78 +6,76 @@
 using namespace cps;
 using namespace std;
 
-#define ok CHECK
-
 SCENARIO("future as a shared pointer", "[shared]") {
 	GIVEN("an empty future") {
 		auto f = future<int>::create_shared("some future");
-		ok(!f->is_ready());
-		ok(!f->is_done());
-		ok(!f->is_failed());
-		ok(!f->is_cancelled());
-		ok(f->current_state() == "pending");
-		ok(f->label() == "some future");
+		CHECK(!f->is_ready());
+		CHECK(!f->is_done());
+		CHECK(!f->is_failed());
+		CHECK(!f->is_cancelled());
+		CHECK(f->current_state() == "pending");
+		CHECK(f->label() == "some future");
 		WHEN("marked as done") {
 			f->done(123);
 			THEN("state is correct") {
-				ok( f->is_ready());
-				ok( f->is_done());
-				ok(!f->is_failed());
-				ok(!f->is_cancelled());
-				ok(f->current_state() == "done");
+				CHECK( f->is_ready());
+				CHECK( f->is_done());
+				CHECK(!f->is_failed());
+				CHECK(!f->is_cancelled());
+				CHECK(f->current_state() == "done");
 			}
 			AND_THEN("elapsed is nonzero") {
-				ok(f->elapsed().count() > 0);
+				CHECK(f->elapsed().count() > 0);
 			}
 			AND_THEN("description looks about right") {
-				ok(string::npos != f->describe().find("some future (done), "));
+				CHECK(string::npos != f->describe().find("some future (done), "));
 			}
 			auto weak = std::weak_ptr<cps::future<int>>(f);
 			f.reset();
 			AND_THEN("shared_ptr goes away correctly") {
-				ok(weak.expired());
+				CHECK(weak.expired());
 			}
 		}
 		WHEN("marked as failed") {
 			f->fail("...");
 			THEN("state is correct") {
-				ok( f->is_ready());
-				ok(!f->is_done());
-				ok( f->is_failed());
-				ok(!f->is_cancelled());
-				ok(f->current_state() == "failed");
+				CHECK( f->is_ready());
+				CHECK(!f->is_done());
+				CHECK( f->is_failed());
+				CHECK(!f->is_cancelled());
+				CHECK(f->current_state() == "failed");
 			}
 			AND_THEN("elapsed is nonzero") {
-				ok(f->elapsed().count() > 0);
+				CHECK(f->elapsed().count() > 0);
 			}
 			AND_THEN("description looks about right") {
-				ok(string::npos != f->describe().find("some future (failed), "));
+				CHECK(string::npos != f->describe().find("some future (failed), "));
 			}
 			auto weak = std::weak_ptr<cps::future<int>>(f);
 			f.reset();
 			AND_THEN("shared_ptr goes away correctly") {
-				ok(weak.expired());
+				CHECK(weak.expired());
 			}
 		}
 		WHEN("marked as cancelled") {
 			f->cancel();
 			THEN("state is correct") {
-				ok( f->is_ready());
-				ok(!f->is_done());
-				ok(!f->is_failed());
-				ok( f->is_cancelled());
-				ok(f->current_state() == "cancelled");
+				CHECK( f->is_ready());
+				CHECK(!f->is_done());
+				CHECK(!f->is_failed());
+				CHECK( f->is_cancelled());
+				CHECK(f->current_state() == "cancelled");
 			}
 			AND_THEN("elapsed is nonzero") {
-				ok(f->elapsed().count() > 0);
+				CHECK(f->elapsed().count() > 0);
 			}
 			AND_THEN("description looks about right") {
-				ok(string::npos != f->describe().find("some future (cancelled), "));
+				CHECK(string::npos != f->describe().find("some future (cancelled), "));
 			}
 			auto weak = std::weak_ptr<cps::future<int>>(f);
 			f.reset();
 			AND_THEN("shared_ptr goes away correctly") {
-				ok(weak.expired());
+				CHECK(weak.expired());
 			}
 		}
 	}
@@ -99,7 +97,7 @@ SCENARIO("failed future handling", "[shared]") {
 		WHEN("we call ->failure_reason") {
 			auto reason = f->failure_reason();
 			THEN("we get the failure reason") {
-				ok(reason == "some reason");
+				CHECK(reason == "some reason");
 			}
 		}
 		WHEN("we call ->value") {
@@ -140,7 +138,7 @@ SCENARIO("successful future handling", "[shared]") {
 		}
 		WHEN("we call ->value") {
 			THEN("we get the original value") {
-				ok(f->value() == "all good");
+				CHECK(f->value() == "all good");
 			}
 		}
 	}
@@ -177,306 +175,6 @@ SCENARIO("cancelled future handling", "[shared]") {
 				CHECK(ec == cps::future_errc::is_cancelled);
 				CHECK(ec.category() == cps::future_category);
 				CHECK(ec.category().name() == std::string { "cps::future" });
-			}
-		}
-	}
-}
-
-SCENARIO("needs_all", "[composed][shared]") {
-	GIVEN("an empty list of futures") {
-		auto na = needs_all();
-		WHEN("we check status") {
-			THEN("it reports as complete") {
-				ok(na->is_done());
-			}
-		}
-	}
-	GIVEN("some pending futures") {
-		auto f1 = future<int>::create_shared();
-		auto f2 = future<int>::create_shared();
-		auto na = needs_all(f1, f2);
-		ok(!na->is_ready());
-		ok(!na->is_done());
-		ok(!na->is_failed());
-		ok(!na->is_cancelled());
-		WHEN("f1 marked as done") {
-			f1->done(123);
-			THEN("needs_all is still pending") {
-				ok(!na->is_ready());
-			}
-		}
-		WHEN("f2 marked as done") {
-			f2->done(123);
-			THEN("needs_all is still pending") {
-				ok(!na->is_ready());
-			}
-		}
-		WHEN("all dependents marked as done") {
-			f1->done(34);
-			f2->done(123);
-			THEN("needs_all is complete") {
-				ok(na->is_done());
-			}
-		}
-		WHEN("a dependent fails") {
-			f1->fail("...");
-			THEN("needs_all is now failed") {
-				ok(na->is_failed());
-			}
-		}
-		WHEN("a dependent is cancelled") {
-			f1->cancel();
-			THEN("needs_all is now failed") {
-				ok(na->is_failed());
-			}
-		}
-	}
-}
-
-SCENARIO("we can chain futures via ->then", "[composed][shared]") {
-	GIVEN("a two-item ->then chain") {
-		auto f1 = cps::make_future<string>();
-		auto f2 = cps::make_future<string>();
-		bool called = false;
-		auto seq = f1->then([f2, &called](string v) -> shared_ptr<future<string>> {
-			ok(v == "input");
-			called = true;
-			return f2;
-		});
-		WHEN("dependent completes") {
-			f1->done("input");
-			THEN("our callback was called") {
-				ok(called);
-			}
-			AND_THEN("->then result is unchanged") {
-				ok(!seq->is_ready());
-			}
-			f2->done("inner");
-			AND_THEN("->then is now complete") {
-				ok(seq->is_done());
-			}
-			AND_THEN("and propagated the value") {
-				ok(seq->value() == "inner");
-			}
-		}
-	}
-}
-
-SCENARIO("we can handle dependent failure in ->then", "[composed][shared]") {
-	GIVEN("a two-item ->then chain") {
-		auto f1 = cps::make_future<string>();
-		auto f2 = cps::make_future<string>();
-		bool called = false;
-		auto seq = f1->then([f2, &called](string v) -> shared_ptr<future<string>> {
-			ok(v == "input");
-			called = true;
-			return f2;
-		});
-		WHEN("dependent fails") {
-			f1->fail("breakage");
-			THEN("our callback was not called") {
-				ok(!called);
-				AND_THEN("->then result is also failed") {
-					ok(seq->is_failed());
-					ok(seq->failure_reason() == f1->failure_reason());
-				}
-			}
-		}
-	}
-}
-
-SCENARIO("we can handle cancellation in ->then", "[composed][shared]") {
-	GIVEN("a two-item ->then chain") {
-		auto f1 = cps::make_future<string>();
-		auto f2 = cps::make_future<string>();
-		bool called = false;
-		auto seq = f1->then([f2, &called](string v) -> shared_ptr<future<string>> {
-			ok(v == "input");
-			called = true;
-			return f2;
-		});
-		WHEN("dependent is cancelled") {
-			f1->cancel();
-			THEN("our callback was not called") {
-				ok(!called);
-			}
-			AND_THEN("->then result is also cancelled") {
-				ok(seq->is_cancelled());
-			}
-		}
-	}
-}
-
-SCENARIO("we can cancel the future returned by ->then", "[composed][shared]") {
-	GIVEN("a two-item ->then chain") {
-		auto f1 = cps::make_future<string>();
-		auto f2 = cps::make_future<string>();
-		bool called = false;
-		auto seq = f1->then([f2, &called](string v) -> shared_ptr<future<string>> {
-			ok(v == "input");
-			called = true;
-			return f2;
-		});
-		WHEN("sequence future is cancelled") {
-			seq->cancel();
-			THEN("our callback was not called") {
-				ok(!called);
-			}
-			AND_THEN("->then result is cancelled") {
-				ok(seq->is_cancelled());
-			}
-			AND_THEN("leaf future was not touched") {
-				ok(!f1->is_ready());
-			}
-		}
-	}
-}
-
-SCENARIO("we can remap errors via ->then", "[composed][shared]") {
-	GIVEN("a simple ->then chain with std::exception handler") {
-		auto initial = cps::future<string>::create_shared();
-		auto success = cps::future<string>::create_shared();
-		auto failure = cps::future<string>::create_shared();
-		bool called = false;
-		bool errored = false;
-		auto seq = initial->then([success, &called](string v) -> shared_ptr<future<string>> {
-			ok(v == "input");
-			called = true;
-			return success;
-		}, [failure, &errored](const std::exception &) {
-			errored = true;
-			return failure;
-		});
-		WHEN("dependent completes") {
-			auto weak = std::weak_ptr<cps::future<string>>(failure);
-			initial->done("input");
-			THEN("our callback was called") {
-				ok(called);
-				ok(!errored);
-			}
-			AND_THEN("->then result is unchanged") {
-				ok(!seq->is_ready());
-			}
-			failure.reset();
-			AND_THEN("other future pointer expired") {
-				ok(weak.expired());
-			}
-		}
-		WHEN("dependent fails") {
-			auto weak = std::weak_ptr<cps::future<string>>(success);
-			initial->fail("error");
-			THEN("our error handler was called") {
-				ok(!called);
-				ok(errored);
-			}
-			AND_THEN("->then result is unchanged") {
-				ok(!seq->is_ready());
-			}
-			success.reset();
-			AND_THEN("other future pointer expired") {
-				ok(weak.expired());
-			}
-		}
-		WHEN("->then cancelled") {
-			auto weak1 = std::weak_ptr<cps::future<string>>(success);
-			auto weak2 = std::weak_ptr<cps::future<string>>(failure);
-			seq->cancel();
-			THEN("neither handler was called") {
-				ok(!called);
-				ok(!errored);
-			}
-			AND_THEN("->then is marked as cancelled") {
-				ok(seq->is_cancelled());
-			}
-			success.reset();
-			failure.reset();
-			// FIXME These should expire immediately,
-			// we should not have to mark the initial
-			// future as ready first
-			initial->cancel();
-			AND_THEN("both future pointers expired") {
-				ok(weak1.expired());
-				ok(weak2.expired());
-			}
-		}
-	}
-}
-
-struct CustomException : public runtime_error { using runtime_error::runtime_error; };
-
-SCENARIO("->then with multiple branches") {
-	GIVEN("a simple chained future") {
-		auto initial = cps::make_future<string>();
-		auto seq = initial->then([](string v) -> shared_ptr<future<string>> {
-			return cps::resolved_future<string>("original: " + v);
-		}, [](const CustomException &) {
-			return cps::resolved_future<string>("from a custom exception");
-		}, [](const std::runtime_error &) {
-			return cps::resolved_future<string>("from std::runtime_error");
-		}, [](const std::exception &) {
-			return cps::resolved_future<string>("from std::exception");
-		});
-		WHEN("we succeed") {
-			initial->done("valid");
-			THEN("things look good") {
-				CHECK(seq->value() == "original: valid");
-			}
-		}
-		WHEN("we fail with a generic exception") {
-			initial->fail(std::exception { });
-			THEN("we see that exception") {
-				CHECK(seq->value() == "from std::exception");
-			}
-		}
-		WHEN("we fail with a runtime error") {
-			initial->fail(std::runtime_error { "hello" });
-			THEN("we see that exception") {
-				CHECK(seq->value() == "from std::runtime_error");
-			}
-		}
-		WHEN("we fail with a custom exception") {
-			initial->fail(CustomException { "me too" });
-			THEN("we see that exception") {
-				CHECK(seq->value() == "from a custom exception");
-			}
-		}
-	}
-}
-
-SCENARIO("exception within ->then branches") {
-	GIVEN("a simple chained future") {
-		auto initial = cps::make_future<string>();
-		auto seq = initial->then([](string v) -> shared_ptr<future<string>> {
-			throw std::runtime_error("ok branch");
-		}, [](const CustomException &) -> shared_ptr<future<string>> {
-			throw std::runtime_error("custom exception branch");
-		}, [](const std::runtime_error &) -> shared_ptr<future<string>> {
-			throw std::runtime_error("runtime_error branch");
-		}, [](const std::exception &) -> shared_ptr<future<string>> {
-			throw std::runtime_error("exception branch");
-		});
-		WHEN("we succeed") {
-			initial->done("valid");
-			THEN("things look good") {
-				CHECK(seq->failure_reason() == "ok branch");
-			}
-		}
-		WHEN("we fail with a generic exception") {
-			initial->fail(std::exception { });
-			THEN("we see that exception") {
-				CHECK(seq->failure_reason() == "exception branch");
-			}
-		}
-		WHEN("we fail with a runtime error") {
-			initial->fail(std::runtime_error { "hello" });
-			THEN("we see that exception") {
-				CHECK(seq->failure_reason() == "runtime_error branch");
-			}
-		}
-		WHEN("we fail with a custom exception") {
-			initial->fail(CustomException { "me too" });
-			THEN("we see that exception") {
-				CHECK(seq->failure_reason() == "custom exception branch");
 			}
 		}
 	}
